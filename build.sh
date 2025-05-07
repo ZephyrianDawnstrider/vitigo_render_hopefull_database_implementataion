dc42762f800ce85679e8c07e92f22fb5112b0d8a
Ayush Chaturvedi <ayushchaturvedi405@gmail.com>
2025-05-07 15:45:41 +0530

    .7

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Install Microsoft ODBC Driver 17 for SQL Server
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17
+
 # Install dependencies
 pip install -r requirements.txt
 
